53385c3fe1615096677742f322c470569e1dfa67
raissa santana <raissasilmesant@gmail.com>
2020-09-23 15:36:53 -0300

    Add columns,validations in User and update sign up's form

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
+   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :name, :address, :ni, presence: true
+  validates :ni, uniqueness: true, format: {with: /((\d{14})|(\d{11}))/, message: "Invalid. Try again, please." }
 end
