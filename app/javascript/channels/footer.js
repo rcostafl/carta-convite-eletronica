a008cc7a385c3423bded0c2ffe0741f3b91f21a4
raissa santana <raissasilmesant@gmail.com>
2020-09-27 11:11:05 -0300

    footer doesnt hide content anymore. Bug solved with js.

--- app/javascript/channels/footer.js
+++ app/javascript/channels/footer.js
@@ -1,9 +1,13 @@
-const footer = document.querySelector('.footer')
+
 
 const hideFooter= () => {
+    const footer = document.querySelector('.footer');
     window.addEventListener ('scroll', () =>  {
         if ((window.innerHeight + window.pageYOffset) >= document.body.offsetHeight) {
             footer.style.visibility = 'hidden';
+        }
+        else {
+            footer.style.visibility = 'visible';
         }
     });
 }
